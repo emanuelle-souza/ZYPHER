6fbb220d15758dc8eccff3ee0acc36520c04845c
emanuelle-souza <emanuelle.s.lima6@aluno.senai.br>
2025-11-07 14:20:58 -0300

    termos
    arrumei

--- VIEWS/HomeCliente.php
+++ VIEWS/HomeCliente.php
@@ -82,7 +82,7 @@ $produtos = ProdutoController::listarProdutos();
     
  <footer>
     <a href="/zypher/VIEWS/Politicas.php">Política de Privacidade</a> | 
-    <a href="#">Termos de Uso</a> | 
+    <a href="/zypher/VIES/Termos.php">Termos de Uso</a> | 
     <a href="/zypher/VIEWS/FaleConosco.php">Fale conosco</a>
     <p>&copy; 2025 Zypher Sneakers. Todos os direitos reservados.</p>
 </footer>
