f9d077708d5c48143a6b15831bba2360b2b0e4b1
kay-kaylane <kaylane.miranda@aluno.senai.br>
2025-11-26 13:10:28 -0300

    update adm

--- VIEWS/LoginAdministrador.php
+++ VIEWS/LoginAdministrador.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login cliente</title>
+    <title>Login ADM</title>
     <link rel="stylesheet" href="/zypher/CSS/LoginAdm.css">
 </head>
 <body>
